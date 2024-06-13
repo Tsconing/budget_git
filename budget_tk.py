d1b5bffc4c8d123be448ecf6e2160cbffb7a1f6d
Tsconing <102267394+Tsconing@users.noreply.github.com>
2024-06-12 22:17:20 -0400

    adding labels, textfield widgets

--- budget_tk.py
+++ budget_tk.py
@@ -5,10 +5,30 @@ from tkinter import ttk
 
 
 root = Tk()
+root.title("Budget Manager GUI")
 
+label1 = ttk.Label(root, text="Label 1")
+label1.grid(row=0, column=0, padx=5, pady=5)
+
+entry1 = ttk.Entry(root, width=30)
+entry1.grid(row=0, column=1, padx=5, pady=5)
+
+label2 = ttk.Label(root, text="Label 2")
+label2.grid(row=1, column=0, padx=5, pady=5)
+
+entry2 = ttk.Entry(root, width=30)
+entry2.grid(row=1, column=1, padx=5, pady=5)
+
+label3 = ttk.Label(root, text="Label 3")
+label3.grid(row=2, column=0, padx=5, pady=5)
+
+entry3 = ttk.Entry(root, width=30)
+entry3.grid(row=2, column=1, padx=5, pady=5)
 
 button = ttk.Button(root, text="This is my button")
+button.grid(row=3, column=0, padx=5, pady=5, sticky='SE')
 
-button.pack()
+button.config(text='PUSH me pls')
+
 
 root.mainloop()
